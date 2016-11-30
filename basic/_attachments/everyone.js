b99999661635e6634b5498b8a6593fc2872b2132
Estelle Comment <estelle.comment@gmail.com>
2016-11-30 10:02:57 +0100

    Display everyone's position on map.

--- basic/_attachments/everyone.js
+++ basic/_attachments/everyone.js
@@ -1,3 +1,6 @@
+var map;
+var bounds;
+var markers;
 $(document).ready(function() {
   let getLatestLocations = function() {
     return db.query('basic/locations', { include_docs: true }).then(function (res) {
@@ -35,6 +38,40 @@ $(document).ready(function() {
         '</li>';
   };
 
+  let makeMarker = function(map, markers, place) {
+    if (!place || !place.geometry || !place.geometry.location) {
+      return;
+    }
+    var icon;
+    if (place.icon) {
+      icon = {
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25),
+        url: place.icon
+      };
+    }
+    markers.push(new google.maps.Marker({
+      icon: icon,
+      map: map,
+      title: place.name,
+      position: place.geometry.location
+    }));
+  };
+
+  let addBounds = function(bounds, place){
+    if (!place || !place.geometry) {
+      return;
+    }
+    if (place.geometry.viewport) {
+      // Only geocodes have viewport.
+      bounds.union(place.geometry.viewport);
+    } else if (place.geometry.location) {
+      bounds.extend(place.geometry.location);
+    }    
+  }
+
   Promise.all([getUsers(), getLatestLocations()])
     .then(function(res) {
       let users = res[0];
@@ -44,10 +81,28 @@ $(document).ready(function() {
 
       var userList = $('#userlist');
       for (let user of users) {
-        userList.append(fillUserLocationTemplate(user, latestLocations[user._id]));
+        if (latestLocations[user._id]) {
+          userList.append(fillUserLocationTemplate(user, latestLocations[user._id]));
+          makeMarker(map, markers, latestLocations[user._id].place);
+          addBounds(bounds, latestLocations[user._id].place);
+        }
       }
+      map.fitBounds(bounds);
     })    
     .catch(function(err) {
       console.error(err);
     });
 });
+
+function initMap() {
+  var uluru = {
+    lat: -25.363,
+    lng: 131.044
+  };
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  bounds = new google.maps.LatLngBounds();
+  markers = [];
+}
--- basic/_attachments/mapSearchBox.js
+++ basic/_attachments/mapSearchBox.js
@@ -22,6 +22,40 @@ function initMapSearchBox() {
   });
 
   var markers = [];
+  let makeMarker = function(map, markers, place) {
+    if (!place || !place.geometry || !place.geometry.location) {
+      return;
+    }
+    var icon;
+    if (place.icon) {
+      icon = {
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25),
+        url: place.icon
+      };
+    }
+    markers.push(new google.maps.Marker({
+      icon: icon,
+      map: map,
+      title: place.name,
+      position: place.geometry.location
+    }));
+  };
+
+  let addBounds = function(bounds, place){
+    if (!place || !place.geometry) {
+      return;
+    }
+    if (place.geometry.viewport) {
+      // Only geocodes have viewport.
+      bounds.union(place.geometry.viewport);
+    } else if (place.geometry.location) {
+      bounds.extend(place.geometry.location);
+    }    
+  }
+
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
   searchBox.addListener('places_changed', function() {
@@ -49,28 +83,8 @@ function initMapSearchBox() {
         console.log("Returned place contains no geometry");
         return;
       }
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
-
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
+      makeMarker(map, markers, place);
+      addBounds(bounds, place);
     });
     map.fitBounds(bounds);
   });
@@ -82,24 +96,35 @@ function savePlace(){
 
   var name = getInputs('#newLocationForm').name;
   var place = selectedPlace;
-  console.log('saving place', name, selectedPlace);  
+  console.log('saving place', name, place);  
 
   // Validate inputs.
   if (!name || name === "") {
     displayMessage('Enter a user name please.');
     return;
   }
-  if (!selectedPlace || $.isEmptyObject(selectedPlace) ||
-      !selectedPlace.geometry || !selectedPlace.geometry.location ||
-      !selectedPlace.name ||
-      !selectedPlace.formatted_address) {
+  if (!place || $.isEmptyObject(place) ||
+      !place.geometry || !place.geometry.location ||
+      !place.name ||
+      !place.formatted_address) {
     displayMessage('Select a place please.');
     return;    
   }
   var savedPlace = {
-    latLng: { lat: place.geometry.location.lat(), lng: place.geometry.location.lng() },
-    name: selectedPlace.name,
-    formatted_address: selectedPlace.formatted_address
+    geometry: {
+      location: {
+        lat: place.geometry.location.lat(),
+        lng: place.geometry.location.lng()
+      },
+      viewport: {
+        east: place.geometry.viewport.b.f,
+        north: place.geometry.viewport.f.b,
+        west: place.geometry.viewport.b.b,
+        south: place.geometry.viewport.f.f
+      }
+    },
+    name: place.name,
+    formatted_address: place.formatted_address,
   };
   console.log(savedPlace);
   submitLocation(name, savedPlace);
